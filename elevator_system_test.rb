94548ddb37c0c2a8fcadee7c934c4c3a839bcc9c
Andrew Nicholson <andrew.nicholson4491@gmail.com>
2020-11-23 14:25:11 -0500

    add elevator system test to exercise ElevatorSystem

---
+++ elevator_system_test.rb
@@ -0,0 +1,22 @@
+require 'minitest/autorun'
+require './elevator_system'
+
+class ElevatorSystemTest < Minitest::Test
+  def test_elevator_system
+    elevator_system = ElevatorSystem.new(elevators: 3, floors: 10)
+    elevator_system.elevator_request(floor: 1, direction: :up)
+    elevator_system.time_passed
+    elevator_system.time_passed
+    elevator_system.time_passed
+    # "Passenger(s) picked up from floor 1"
+    elevator_system.floor_request(7)
+    elevator_system.floor_request(9)
+    elevator_system.time_passed
+    elevator_system.time_passed
+    elevator_system.time_passed
+    elevator_system.time_passed
+    # "Passenger(s) dropped off on floor 7"
+    elevator_system.time_passed
+    # "Passenger(s) dropped off on floor 9"
+  end
+end
